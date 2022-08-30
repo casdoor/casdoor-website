7c5c40e93c49f92809e95e429c6677b73ecac428
YunShu <im@yunshu.site>
2022-08-30 23:46:08 +0800

    refactor: Switch to Giscus commenting system (#316)

--- src/theme/DocPaginator/index.js
+++ src/theme/DocPaginator/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import DocPaginator from '@theme-original/DocPaginator';
-import { DiscussionEmbed } from 'disqus-react';
+import Giscus from '@giscus/react';
+import { useColorMode } from '@docusaurus/theme-common';
 
 export default function DocPaginatorWrapper(props) {
+  const { colorMode } = useColorMode();
   return (
     <>
       <DocPaginator {...props} />
-      <DiscussionEmbed
-        shortname='casdoor'
-        config={
-          {
-            language: 'en'
-          }
-        }
+      <br />
+      <Giscus
+        id="comments"
+        repo="casdoor/casdoor"
+        repoId="MDEwOlJlcG9zaXRvcnkzMDYzNjY5MDA="
+        category="Docs comments"
+        categoryId="DIC_kwDOEkLJtM4CRIiC"
+        mapping="pathname"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme={colorMode}
+        lang="en"
+        loading="lazy"
       />
     </>
   );
