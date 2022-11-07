8c3002c2306d33e4619c9f1c29639e86586c43f6
Dinn <ding526123@gmail.com>
2022-11-07 17:47:05 +0800

    fix(FrameMask): The online demo has an error in the jump link (#388)
    * fix(FrameMask): The online demo has an error in the jump link
    
    * Update src/pages/index.js
    
    Co-authored-by: Yaodong Yu <2814461814@qq.com>
    
    Co-authored-by: Yaodong Yu <2814461814@qq.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,11 +40,11 @@ function FrameMask(props) {
     });
   };
 
-  const handleClick = ({src}) => {
-    if(localStorage.getItem("mainland") === "true") {
-      window.open("https://dooc.casdoor.com" + src);
-    }else{
-      window.open("https://door.casdoor.org" + src);
+  const handleClick = (src) => {
+    if (localStorage.getItem("mainland") === "true") {
+      window.open("https://door.casdoor.com/" + src);
+    } else {
+      window.open("https://door.casdoor.org/" + src);
     }
   };
 
