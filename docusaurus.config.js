d962250a9eba2284b26179c2c80c116a19e19ae8
Gucheng Wang <nomeguy@qq.com>
2023-01-19 20:51:40 +0800

    Fix discord link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -148,7 +148,7 @@ module.exports = {
             },
             {
               label: "Discord",
-              href: "https://discord.gg/qteNGWt8UY",
+              href: "https://discord.com/invite/qteNGWt8UY",
             },
             {
               label: "QQ Group",
