b3293b7d92700e23941de08717f2882fe0d5004a
Gucheng <85475922+nomeguy@users.noreply.github.com>
2023-07-22 21:16:15 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -214,10 +214,10 @@ module.exports = {
           items: [
             "provider/captcha/overview",
             "provider/captcha/default",
+            "provider/captcha/cloudflareTurnstile",
             "provider/captcha/recaptcha",
             "provider/captcha/hcaptcha",
             "provider/captcha/aliyunCaptcha",
-            "provider/captcha/cloudflareTurnstile",
             "provider/captcha/geetest",
           ],
         },
