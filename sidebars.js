ffb8f47c61dcb4bbda938c3f638b52b05a2197ac
haiwu <likamylike@gmail.com>
2023-09-07 17:04:17 +0800

    docs: support wechat pay

--- sidebars.js
+++ sidebars.js
@@ -247,7 +247,7 @@ module.exports = {
             "provider/payment/paypal",
             "provider/payment/stripe",
             "provider/payment/Alipay",
-            "provider/payment/WeChatPay",
+            "provider/payment/wechatpay",
           ],
         },
         {
