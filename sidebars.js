28f42d0dd40df8d971ea666e0da7ef35b2ce78f5
haiwu <54203997+Chinoholo0807@users.noreply.github.com>
2023-10-21 17:56:06 +0800

    docs: add SAML Custom Provider (#580)

--- sidebars.js
+++ sidebars.js
@@ -239,6 +239,7 @@ module.exports = {
             "provider/saml/overview",
             "provider/saml/aliyun",
             "provider/saml/keycloak",
+            "provider/saml/custom",
           ],
         },
         {
