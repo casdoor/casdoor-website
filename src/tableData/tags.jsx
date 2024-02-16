76dd85853d8e65278042489b07dd6932945472b5
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2024-02-16 21:28:20 +0800

    add ecosystem page

---
+++ src/tableData/tags.jsx
@@ -0,0 +1,212 @@
+import {translate} from "@docusaurus/Translate";
+
+export const Tags = {
+  Integration: {
+    label: translate({message: "Integration"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  OAuth: {
+    label: translate({message: "OAuth"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  Email: {
+    label: translate({message: "OAuth"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  SMS: {
+    label: translate({message: "OAuth"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  Notification: {
+    label: translate({message: "OAuth"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  Storage: {
+    label: translate({message: "OAuth"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  SAML: {
+    label: translate({message: "OAuth"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  Captcha: {
+    label: translate({message: "OAuth"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  Web3: {
+    label: translate({message: "OAuth"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  Provider: {
+    label: translate({message: "Go"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  Cpp: {
+    label: translate({message: "Cpp"}),
+    description: translate({
+      message:
+                "Cpp language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00599C",
+  },
+  Csharp: {
+    label: translate({message: "Cpp"}),
+    description: translate({
+      message:
+                "Cpp language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00599C",
+  },
+  Go: {
+    label: translate({message: "Go"}),
+    description: translate({
+      message: "go language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#00ADD8",
+  },
+  Java: {
+    label: translate({message: "Java"}),
+    description: translate({
+      message:
+                "Java language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#007396",
+  },
+  JavaScript: {
+    label: translate({message: "NodeJS"}),
+    description: translate({
+      message: "NodeJS runtime",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#339933",
+  },
+  Lua: {
+    label: translate({message: "Lua"}),
+    description: translate({
+      message:
+                "Lua language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#000080",
+  },
+  PHP: {
+    label: translate({message: "PHP"}),
+    description: translate({
+      message:
+                "PHP language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#777BB4",
+  },
+
+  Ruby: {
+    label: translate({message: "Ruby"}),
+    description: translate({
+      message:
+                "Ruby language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#CC342D",
+  },
+  Haskell: {
+    label: translate({message: "Ruby"}),
+    description: translate({
+      message:
+                "Ruby language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#CC342D",
+  },
+  Python: {
+    label: translate({message: "Python"}),
+    description: translate({
+      message:
+                "Python Language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#3776AB",
+  },
+  SDK: {
+    label: translate({message: "dotNET"}),
+    description: translate({
+      message:
+                ".NET runtime",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#512BD4",
+  },
+  Mobile: {
+    label: translate({message: "Rust"}),
+    description: translate({
+      message:
+                "Rust language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#000000",
+  },
+
+  Desktop: {
+    label: translate({message: "Swift"}),
+    description: translate({
+      message:
+                "Swift language",
+      id: "showcase.tag.favorite.description",
+    }),
+    color: "#FA7343",
+  },
+
+  Web: {
+    label: translate({message: "adapter"}),
+    description: translate({
+      message: "Adapter",
+      id: "showcase.tag.opensource.description",
+    }),
+    color: "#FFC107",
+  },
+};
+
+export const TagList = Object.keys(Tags);
